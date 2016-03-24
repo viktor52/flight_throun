d5b0952424eb134be5a04eaed1df87e5f9d5f4e5
vitkor52 <viktorymir91@gmail.com>
2016-03-24 14:21:00 +0000

    'test'

--- src/search/DB_Connection.java
+++ src/search/DB_Connection.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 public class DB_Connection {
 	private static Connection c = null;
     private static Statement stmt = null;
-    private static String url = "jdbc:postgresql://localhost:5432/plains";
+    private static String url = "jdbc:postgresql://localhost:5432/planes";
     private static String user = "viktor";
 	public static String findDep(){ 
     String s = null;
@@ -18,7 +18,7 @@ public class DB_Connection {
        stmt = c.createStatement();
 	      ResultSet rs = stmt.executeQuery( "SELECT * FROM flight;" );
 	      while ( rs.next() ) {
-	            s = rs.getString("number") + rs.getString("airport");
+	            s = rs.getString("number") +"   "+ rs.getString("airport");
 	         }
     } catch (Exception e) {
        e.printStackTrace();
