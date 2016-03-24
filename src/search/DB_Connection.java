93d49a6a8b3532a7f0df7e3912f6f7ac17c7d227
vitkor52 <viktorymir91@gmail.com>
2016-03-24 14:24:04 +0000

    haha

--- src/search/DB_Connection.java
+++ src/search/DB_Connection.java
@@ -9,12 +9,14 @@ public class DB_Connection {
 	private static Connection c = null;
     private static Statement stmt = null;
     private static String url = "jdbc:postgresql://localhost:5432/planes";
-    private static String user = "viktor";
-	public static String findDep(){ 
+    private static String user = "";
+    private static String pass = "";
+	
+    public static String findDep(){ 
     String s = null;
     try {
        Class.forName("org.postgresql.Driver");
-       c = DriverManager.getConnection(url, user, "");
+       c = DriverManager.getConnection(url, user, pass);
        stmt = c.createStatement();
 	      ResultSet rs = stmt.executeQuery( "SELECT * FROM flight;" );
 	      while ( rs.next() ) {
